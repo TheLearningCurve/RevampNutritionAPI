36039d6381ebef7e2ab32cfbd350cf03d893397b
Brandon VanderMey <brandon.vandermey@gmail.com>
2015-05-28 00:19:40 -0400

    Changes I've made so far...

---
+++ src/nutritionAPIV2_controllers/ItemButton.java
@@ -0,0 +1,12 @@
+package nutritionAPIV2_controllers;
+
+import javafx.scene.image.ImageView;
+
+public class ItemButton extends ImageView
+{
+	private String brandLogoUrl;
+	private String brandName;
+	private String itemName;
+	
+	public ItemButton() { }
+}
