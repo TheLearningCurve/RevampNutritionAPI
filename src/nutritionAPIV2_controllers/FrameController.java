8a402f51d9cb096b7abf32f08b2e7048bc131830
Brandon VanderMey <brandon.vandermey@gmail.com>
2015-06-08 23:31:05 -0400

    Left Panel Buttons

--- src/nutritionAPIV2_controllers/FrameController.java
+++ src/nutritionAPIV2_controllers/FrameController.java
@@ -1,7 +1,8 @@
 package nutritionAPIV2_controllers;
 
-import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -9,14 +10,11 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
-import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 
 public class FrameController implements Initializable
@@ -58,8 +56,13 @@ public class FrameController implements Initializable
 	ObservableList<String> items =FXCollections.observableArrayList ( 
 		    "Single", "Double", "Suite", "Single", "Double", "Suite",
 		    "Single", "Double", "Suite", "Single", "Double", "Suite");
-		
-
+	
+	int numberOfButtons = 0;
+	static int buttonNumberPressed = -1;
+	
+	Group buttonGroup = new Group();
+	
+	static List<ItemButton> buttons = new ArrayList<ItemButton>();
 	
 	@FXML
 	public void getTypeAHead()
@@ -70,31 +73,31 @@ public class FrameController implements Initializable
 		
 	}
 	
+	/* Left Panel Code */	
+	
 	@FXML
-	public void addButtons()
+	public void addButtons() // Change to your liking.
 	{
-		
-		Group g = new Group();
-		for (int i = 0; i < 20; i++)
-		{
-			Button but = new Button(String.valueOf(i));
-			but.setLayoutY(i*40);
-			g.getChildren().add(but);
-			
-		}
-		
-		buttonList.setContent(g);
+			for (int i = 0; i < 10; i++)
+			{
+				ItemButton but = new ItemButton(numberOfButtons);
+				but.setLayoutY(numberOfButtons * 60);
+				buttons.add(but);
+				buttonGroup.getChildren().add(but);
+				numberOfButtons++;
+			}
+	
+			buttonList.setContent(buttonGroup);
 	}
-	
-	/* Left Panel Code */
-	
 
 	@FXML
-	public void finishedScroll()
+	public void finishedScroll() // Change to your liking.
 	{
-		System.out.println("V value: " + buttonList.getVvalue());
-		System.out.println("V max: " + buttonList.getVmax());
+		if (buttonList.getVvalue() == 1)
+			addButtons();
 	}
+	
+	
 	
 	
 	/* Right Panel Code */
